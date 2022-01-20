42a51a654ddfca328cbb85d2906ce74011a16322
Eugene Domotenko <e.domotenko@gmail.com>
2022-01-20 22:33:36 +0200

    Personalize template

--- pages/index.js
+++ pages/index.js
@@ -6,6 +6,7 @@ import { getAllFilesFrontMatter } from '@/lib/mdx'
 import formatDate from '@/lib/utils/formatDate'
 
 import NewsletterForm from '@/components/NewsletterForm'
+import Hero from '@/components/Hero'
 
 const MAX_DISPLAY = 5
 
@@ -19,6 +20,7 @@ export default function Home({ posts }) {
   return (
     <>
       <PageSEO title={siteMetadata.title} description={siteMetadata.description} />
+      <Hero />
       <div className="divide-y divide-gray-200 dark:divide-gray-700">
         <div className="pt-6 pb-8 space-y-2 md:space-y-5">
           <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
