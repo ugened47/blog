ee59cf3073530ea312e308338cf0fe45abba4b83
Eugene Domotenko <e.domotenko@gmail.com>
2022-03-24 00:50:37 +0200

    feat: change site colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -19,7 +19,7 @@ module.exports = {
         sans: ['Inter', ...defaultTheme.fontFamily.sans],
       },
       colors: {
-        primary: colors.rose,
+        primary: colors.yellow,
         gray: colors.neutral,
       },
       typography: (theme) => ({
